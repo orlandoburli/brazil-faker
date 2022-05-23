7fab31de88329e477ea02d86231b04abeb52b492
Orlando Burli Junior <orlando.burli@gmail.com>
2022-05-23 17:01:17 -0400

    Fix constructors to match original

--- src/main/java/br/com/brazil/utils/faker/BrazilFaker.java
+++ src/main/java/br/com/brazil/utils/faker/BrazilFaker.java
@@ -4,13 +4,45 @@ import com.github.javafaker.Faker;
 import com.github.javafaker.service.FakeValuesService;
 import com.github.javafaker.service.RandomService;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class BrazilFaker extends Faker {
 
-  private final BrazilianCompany company;
-  private final BrazilIdNumber idNumber;
+  private BrazilianCompany company;
+  private BrazilIdNumber idNumber;
+
+  public BrazilFaker() {
+    super();
+    this.init();
+  }
+
+  public BrazilFaker(Locale locale) {
+    super(locale, (Random)null);
+    this.init();
+  }
+
+  public BrazilFaker(Random random) {
+    super(Locale.ENGLISH, random);
+    this.init();
+  }
+
+  public BrazilFaker(Locale locale, Random random) {
+    super(locale, new RandomService(random));
+    this.init();
+  }
+
+  public BrazilFaker(Locale locale, RandomService randomService) {
+    super(new FakeValuesService(locale, randomService), randomService);
+    this.init();
+  }
 
   public BrazilFaker(FakeValuesService fakeValuesService, RandomService random) {
     super(fakeValuesService, random);
+    this.init();
+  }
+
+  private void init() {
     this.company = new BrazilianCompany(this);
     this.idNumber = new BrazilIdNumber(this);
   }
@@ -21,6 +53,6 @@ public class BrazilFaker extends Faker {
 
   @Override
   public BrazilIdNumber idNumber() {
-    return this.idNumber();
+    return this.idNumber;
   }
 }
